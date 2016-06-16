0f82ac5cb59a6647ba1c7f50b6c87ff2373ef3b1
Matias Olivera <jmatias.olivera@gmail.com>
2016-06-16 17:39:35 -0300

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def main():
    return render_template('index.html')
+   
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/signOut')
+def showSignUp():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
