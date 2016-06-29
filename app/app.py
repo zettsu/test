c04945b1f0c53efa380dae672390922f5bf80fb4
jolivera <jolivera@antel.net.uy>
2016-06-29 16:13:55 -0300

    commit local

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/dash')
+def main():
+   return render_template('index.html')
+   
+@app.route('/signUp')
+def showSignUp():
+   return render_template('signup.html')
+   
+@app.route('/signIn')
+def showSignIn():
+   return render_template('signin.html')
+
+@app.route('/signOut')
+def showSignOut():
+   return render_template('signout.html')
+
+if __name__ == "__main__":
+   app.run()
