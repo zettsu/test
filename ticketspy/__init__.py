5d796a1440b5171b583521fac91919382120b054
jolivera <jmatias.olivera@gmail.com>
2016-07-13 20:14:37 -0300

    Commit controllers, models

--- ticketspy/__init__.py
+++ ticketspy/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from ticketspy.admin.admin_controller import admin
+from ticketspy.controllers.admin.admin_controller import admin
 
 app.register_blueprint(admin, url_prefix='/admin')
